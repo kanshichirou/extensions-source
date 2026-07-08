d90d16ba14c3c6b337bbdc58226c616c5a1fa889
kanshichirou <hashiramauchiha93@gmail.com>
2026-07-08 09:33:19 +0700

    Update build.gradle.kts

--- src/id/mikoroku/build.gradle.kts
+++ src/id/mikoroku/build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     alias(kei.plugins.extension)
 }
-
 keiyoushi {
     name = "MikoRoku"
-    versionCode = 6
+    versionCode = 7
     contentWarning = ContentWarning.NSFW
     libVersion = "1.4"
     theme = "zeistmanga"
-
     source {
         lang = "id"
         baseUrl = "https://www.mikoroku.com"
